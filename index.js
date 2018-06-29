9cffa71b5e05fd5708d1048367ab07a33ca6d9b1
Celien Nanson <cesliens@gmail.com>
2018-06-29 15:25:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+var shipper = require('lambda-stash');
+
+exports.handler = function(event, context, callback){
+  var srcBucket = event.Records[0].s3.bucket.name;
+
+  var config = {
+    elasticsearch: {
+      host: process.env.ES_INDEXPREFIX,
+      useAWS: false
+    },
+    mappings:
+    [
+      {
+        bucket: srcBucket,
+        processors: [
+          'decompressGzip',
+          'parseJson',
+          'formatCloudtrail',
+          'shipElasticsearch'
+        ],
+        elasticsearch: {
+          index: process.env.ES_INDEXPREFIX + '-' +((new Date()).toJSON().slice(0, 10).replace(/[-T]/g, '.')),
+          type: 'cloudtrail'
+        },
+        dateField: 'date'
+      }
+    ]
+  };
+
+  shipper.handler(config, event, context, callback);
+
+};
